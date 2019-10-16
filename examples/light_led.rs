bc2e428286bb9d33b06800d72e91b5829ebdd673
Vadim Kaushan <admin@disasm.info>
2019-10-17 01:19:26 +0300

    Fix the example

--- examples/light_led.rs
+++ examples/light_led.rs
@@ -8,15 +8,15 @@ use gd32vf103xx_hal as hal;
 use hal::pac as pac;
 use gd32vf103xx_hal::gpio::GpioExt;
 use gd32vf103xx_hal::rcu::RcuExt;
-use embedded_hal::digital::v2::OutputPin;
+use longan_nano::led::Led;
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let mut rcu = dp.RCU.constrain();
     let mut gpioa = dp.GPIOA.split(&mut rcu.apb2);
-    let mut pa1 = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
-    pa1.set_high().unwrap();
+    let mut red_led = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
+    red_led.on();
 
     loop {}
 }
