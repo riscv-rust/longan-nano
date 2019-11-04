f3912432ab53f651cabc61c56451d27f039df2e5
Vadim Kaushan <admin@disasm.info>
2019-11-04 19:23:02 +0300

    Update example to use sprintln macro

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -7,29 +7,18 @@ use riscv_rt::entry;
 use gd32vf103xx_hal as hal;
 use hal::pac as pac;
 use gd32vf103xx_hal::prelude::*;
-use gd32vf103xx_hal::serial::{Serial, Config, Parity, StopBits};
-use core::fmt::Write;
 use gd32vf103xx_hal::clock::Clocks;
+use longan_nano::sprintln;
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let gpioa = dp.GPIOA.split();
 
-
-    let tx = gpioa.pa9.into_alternate_push_pull();
-    let rx = gpioa.pa10.into_floating_input();
-    let serial_config = Config {
-        baudrate: 115_200.bps(),
-        parity: Parity::ParityNone,
-        stopbits: StopBits::STOP1
-    };
     let clocks = Clocks;
-    let serial = Serial::usart0(dp.USART0, (tx, rx), serial_config, clocks);
+    longan_nano::stdout::configure(dp.USART0, gpioa.pa9, gpioa.pa10, 115_200.bps(), clocks);
 
-    let (mut tx, _) = serial.split();
-
-    writeln!(tx, "Hello, world").unwrap();
+    sprintln!("Hello, world");
 
     loop { }
 }
