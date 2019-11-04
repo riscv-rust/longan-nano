af30c62e4e899b3ac60fc2770cad5dd15c2237d9
Vadim Kaushan <admin@disasm.info>
2019-11-04 19:15:21 +0300

    Update examples

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -14,19 +14,18 @@ use gd32vf103xx_hal::clock::Clocks;
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
-    let mut rcu = dp.RCU.constrain();
-    let mut gpioa = dp.GPIOA.split(&mut rcu.apb2);
+    let gpioa = dp.GPIOA.split();
 
 
-    let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.ctl1);
-    let rx = gpioa.pa10.into_floating_input(&mut gpioa.ctl1);
+    let tx = gpioa.pa9.into_alternate_push_pull();
+    let rx = gpioa.pa10.into_floating_input();
     let serial_config = Config {
         baudrate: 115_200.bps(),
         parity: Parity::ParityNone,
         stopbits: StopBits::STOP1
     };
     let clocks = Clocks;
-    let serial = Serial::usart0(dp.USART0, (tx, rx), serial_config, clocks, &mut rcu.apb2);
+    let serial = Serial::usart0(dp.USART0, (tx, rx), serial_config, clocks);
 
     let (mut tx, _) = serial.split();
 
--- examples/light_led.rs
+++ examples/light_led.rs
@@ -7,15 +7,13 @@ use riscv_rt::entry;
 use gd32vf103xx_hal as hal;
 use hal::pac as pac;
 use gd32vf103xx_hal::gpio::GpioExt;
-use gd32vf103xx_hal::rcu::RcuExt;
 use longan_nano::led::Led;
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
-    let mut rcu = dp.RCU.constrain();
-    let mut gpioa = dp.GPIOA.split(&mut rcu.apb2);
-    let mut red_led = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
+    let gpioa = dp.GPIOA.split();
+    let mut red_led = gpioa.pa1.into_push_pull_output();
     red_led.on();
 
     loop {}
