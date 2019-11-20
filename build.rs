0b3832c3fa0b5b050067321f72a4fe2798517e8d
Vadim Kaushan <admin@disasm.info>
2019-11-21 01:01:46 +0300

    Track memory.x changes

--- build.rs
+++ build.rs
@@ -6,4 +6,5 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
     fs::copy("memory.x", out_dir.join("memory.x")).unwrap();
+    println!("cargo:rerun-if-changed=memory.x");
 }
