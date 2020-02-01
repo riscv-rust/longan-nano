6cff8c8eff3258a8d51219c1d00f41e1d84e9633
Vadim Kaushan <admin@disasm.info>
2020-02-01 22:34:48 +0300

    Use lcd API in the examples

--- examples/display.rs
+++ examples/display.rs
@@ -4,17 +4,14 @@
 use panic_halt as _;
 
 use riscv_rt::entry;
-use gd32vf103xx_hal::pac as pac;
+use gd32vf103xx_hal::pac;
 use gd32vf103xx_hal::prelude::*;
-use gd32vf103xx_hal::spi::{Spi, MODE_0};
-use gd32vf103xx_hal::delay::McycleDelay;
-use embedded_hal::digital::v2::OutputPin;
-use st7735_lcd::{ST7735, Orientation};
+use longan_nano::lcd_pins;
+use longan_nano::lcd::Lcd;
 use embedded_graphics::prelude::*;
 use embedded_graphics::fonts::Font6x8;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::primitives::Rectangle;
-
 
 #[entry]
 fn main() -> ! {
@@ -26,24 +23,17 @@ fn main() -> ! {
     let gpioa = dp.GPIOA.split(&mut rcu);
     let gpiob = dp.GPIOB.split(&mut rcu);
 
-    let sck = gpioa.pa5.into_alternate_push_pull();
-    let miso = gpioa.pa6.into_floating_input();
-    let mosi = gpioa.pa7.into_alternate_push_pull();
-    let spi0 = Spi::spi0(dp.SPI0, (sck, miso, mosi), MODE_0, 16.mhz(), &mut rcu);
+    let lcd_pins = lcd_pins!(gpioa, gpiob);
+    let mut lcd = Lcd::new(dp.SPI0, lcd_pins, &mut rcu);
+    let (width, height) = (lcd.width() as i32, lcd.height() as i32);
 
-    let dc = gpiob.pb0.into_push_pull_output();
-    let rst = gpiob.pb1.into_push_pull_output();
-    let mut cs = gpiob.pb2.into_push_pull_output();
-    cs.set_low().unwrap();
+    // Clear screen
+    lcd.draw(Rectangle::new(Coord::new(0, 0), Coord::new(width - 1, height - 1))
+        .fill(Some(Rgb565::from(0u16))));
 
-    let mut lcd = ST7735::new(spi0, dc, rst, false, true);
-    let mut delay = McycleDelay::new(&rcu.clocks);
-    lcd.init(&mut delay).unwrap();
-    lcd.set_orientation(&Orientation::Landscape).unwrap();
-    lcd.set_offset(0, 26);
-
-    lcd.draw(Rectangle::new(Coord::new(0, 0), Coord::new(179, 79)).fill(Some(Rgb565::from(0x0u8))));
-    let t = Font6x8::render_str(" Hello Rust! ").fill(Some(Rgb565::from((0,0xff,0)))).translate(Coord::new(40, 35));
+    let t = Font6x8::render_str(" Hello Rust! ")
+        .fill(Some(Rgb565::from((0,0xff,0))))
+        .translate(Coord::new(40, 35));
     lcd.draw(t);
 
     loop {}
--- examples/ferris.rs
+++ examples/ferris.rs
@@ -6,18 +6,14 @@ use panic_halt as _;
 use riscv_rt::entry;
 use gd32vf103xx_hal::prelude::*;
 use gd32vf103xx_hal::pac;
-use gd32vf103xx_hal::spi::{Spi, MODE_0};
-use gd32vf103xx_hal::delay::McycleDelay;
-use embedded_hal::digital::v2::OutputPin;
-use st7735_lcd::{ST7735, Orientation};
+use longan_nano::lcd_pins;
+use longan_nano::lcd::Lcd;
 use embedded_graphics::prelude::*;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::image::Image16BPP;
 use embedded_graphics::primitives::Rectangle;
 
 const FERRIS: &[u8] = include_bytes!("ferris.raw");
-const LCD_WIDTH: i32 = 180;
-const LCD_HEIGHT: i32 = 80;
 
 #[entry]
 fn main() -> ! {
@@ -29,27 +25,16 @@ fn main() -> ! {
     let gpioa = dp.GPIOA.split(&mut rcu);
     let gpiob = dp.GPIOB.split(&mut rcu);
 
-    let sck = gpioa.pa5.into_alternate_push_pull();
-    let miso = gpioa.pa6.into_floating_input();
-    let mosi = gpioa.pa7.into_alternate_push_pull();
-    let spi0 = Spi::spi0(dp.SPI0, (sck, miso, mosi), MODE_0, 16.mhz(), &mut rcu);
+    let lcd_pins = lcd_pins!(gpioa, gpiob);
+    let mut lcd = Lcd::new(dp.SPI0, lcd_pins, &mut rcu);
+    let (width, height) = (lcd.width() as i32, lcd.height() as i32);
 
-    let dc = gpiob.pb0.into_push_pull_output();
-    let rst = gpiob.pb1.into_push_pull_output();
-    let mut cs = gpiob.pb2.into_push_pull_output();
-    cs.set_low().unwrap();
-
-    let mut lcd = ST7735::new(spi0, dc, rst, false, true);
-    let mut delay = McycleDelay::new(&rcu.clocks);
-    lcd.init(&mut delay).unwrap();
-    lcd.set_orientation(&Orientation::Landscape).unwrap();
-    lcd.set_offset(0, 26);
-
-    let p = Coord::new(LCD_WIDTH as i32 - 1, LCD_HEIGHT as i32 - 1);
-    lcd.draw(Rectangle::new(Coord::new(0, 0), p).fill(Some(Rgb565::from((0,0,0)))));
+    // Clear screen
+    lcd.draw(Rectangle::new(Coord::new(0, 0), Coord::new(width - 1, height - 1))
+        .fill(Some(Rgb565::from(0u16))));
 
     let image: Image<Rgb565, _> = Image16BPP::new(&FERRIS, 86, 64);
-    let image = image.translate(Coord::new(LCD_WIDTH/2 - 43, LCD_HEIGHT/2 - 32));
+    let image = image.translate(Coord::new(width/2 - 43, height/2 - 32));
     lcd.draw(&image);
 
     loop {}
