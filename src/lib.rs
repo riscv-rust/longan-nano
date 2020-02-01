d1779fd6ee346d1e676d0c86c8db968b76f03cdf
Vadim Kaushan <admin@disasm.info>
2020-02-01 22:49:06 +0300

    Add doc_cfg magic

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 //! Board support crate for the Longan Nano board
 
 #![no_std]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 pub use gd32vf103xx_hal as hal;
 
 #[cfg(feature = "lcd")]
+#[cfg_attr(docsrs, doc(cfg(feature = "lcd")))]
 pub mod lcd;
 pub mod led;
 pub mod stdout;
