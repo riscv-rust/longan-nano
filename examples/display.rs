b77175553d94c0183c640915e33c0df99dd2a25e
Jorg Bornschein <jb@capsec.org>
2020-05-05 12:14:27 +0100

    Update to embedded_graphics V0.6 and stm7735 V0.7

--- examples/display.rs
+++ examples/display.rs
@@ -3,39 +3,53 @@
 
 use panic_halt as _;
 
-use riscv_rt::entry;
+use embedded_graphics::fonts::{Font6x8, Text};
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::Rectangle;
+use embedded_graphics::{primitive_style, text_style};
 use gd32vf103xx_hal::pac;
 use gd32vf103xx_hal::prelude::*;
-use longan_nano::lcd_pins;
-use longan_nano::lcd::Lcd;
-use embedded_graphics::prelude::*;
-use embedded_graphics::fonts::Font6x8;
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::primitives::Rectangle;
+use longan_nano::{lcd, lcd_pins};
+use riscv_rt::entry;
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
 
     // Configure clocks
-    let mut rcu = dp.RCU.configure().ext_hf_clock(8.mhz()).sysclk(108.mhz()).freeze();
+    let mut rcu = dp
+        .RCU
+        .configure()
+        .ext_hf_clock(8.mhz())
+        .sysclk(108.mhz())
+        .freeze();
     let mut afio = dp.AFIO.constrain(&mut rcu);
 
     let gpioa = dp.GPIOA.split(&mut rcu);
     let gpiob = dp.GPIOB.split(&mut rcu);
 
     let lcd_pins = lcd_pins!(gpioa, gpiob);
-    let mut lcd = Lcd::new(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
-    let (width, height) = (lcd.width() as i32, lcd.height() as i32);
+    let mut lcd = lcd::configure(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
+    let (width, height) = (lcd.size().width as i32, lcd.size().height as i32);
 
     // Clear screen
-    lcd.draw(Rectangle::new(Coord::new(0, 0), Coord::new(width - 1, height - 1))
-        .fill(Some(Rgb565::from(0u16))));
+    Rectangle::new(Point::new(0, 0), Point::new(width - 1, height - 1))
+        .into_styled(primitive_style!(fill_color = Rgb565::BLACK))
+        .draw(&mut lcd)
+        .unwrap();
 
-    let t = Font6x8::render_str(" Hello Rust! ")
-        .fill(Some(Rgb565::from((0,0xff,0))))
-        .translate(Coord::new(40, 35));
-    lcd.draw(t);
+    let style = text_style!(
+        font = Font6x8,
+        text_color = Rgb565::BLACK,
+        background_color = Rgb565::GREEN
+    );
+
+    // Create a text at position (20, 30) and draw it using style defined above
+    Text::new(" Hello Rust! ", Point::new(40, 35))
+        .into_styled(style)
+        .draw(&mut lcd)
+        .unwrap();
 
     loop {}
 }
--- examples/ferris.rs
+++ examples/ferris.rs
@@ -3,15 +3,16 @@
 
 use panic_halt as _;
 
-use riscv_rt::entry;
-use gd32vf103xx_hal::prelude::*;
-use gd32vf103xx_hal::pac;
-use longan_nano::lcd_pins;
-use longan_nano::lcd::Lcd;
-use embedded_graphics::prelude::*;
+use embedded_graphics::image::{Image, ImageRaw};
+use embedded_graphics::pixelcolor::raw::LittleEndian;
 use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::image::Image16BPP;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitive_style;
 use embedded_graphics::primitives::Rectangle;
+use gd32vf103xx_hal::pac;
+use gd32vf103xx_hal::prelude::*;
+use longan_nano::{lcd, lcd_pins};
+use riscv_rt::entry;
 
 const FERRIS: &[u8] = include_bytes!("ferris.raw");
 
@@ -20,23 +21,32 @@ fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
 
     // Configure clocks
-    let mut rcu = dp.RCU.configure().ext_hf_clock(8.mhz()).sysclk(108.mhz()).freeze();
+    let mut rcu = dp
+        .RCU
+        .configure()
+        .ext_hf_clock(8.mhz())
+        .sysclk(108.mhz())
+        .freeze();
     let mut afio = dp.AFIO.constrain(&mut rcu);
 
     let gpioa = dp.GPIOA.split(&mut rcu);
     let gpiob = dp.GPIOB.split(&mut rcu);
 
     let lcd_pins = lcd_pins!(gpioa, gpiob);
-    let mut lcd = Lcd::new(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
-    let (width, height) = (lcd.width() as i32, lcd.height() as i32);
+    let mut lcd = lcd::configure(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
+    let (width, height) = (lcd.size().width as i32, lcd.size().height as i32);
 
     // Clear screen
-    lcd.draw(Rectangle::new(Coord::new(0, 0), Coord::new(width - 1, height - 1))
-        .fill(Some(Rgb565::from(0u16))));
+    Rectangle::new(Point::new(0, 0), Point::new(width - 1, height - 1))
+        .into_styled(primitive_style!(fill_color = Rgb565::BLACK))
+        .draw(&mut lcd)
+        .unwrap();
 
-    let image: Image<Rgb565, _> = Image16BPP::new(&FERRIS, 86, 64);
-    let image = image.translate(Coord::new(width/2 - 43, height/2 - 32));
-    lcd.draw(&image);
+    // Load Image Data
+    let raw_image: ImageRaw<Rgb565, LittleEndian> = ImageRaw::new(&FERRIS, 86, 64);
+    Image::new(&raw_image, Point::new(width / 2 - 43, height / 2 - 32))
+        .draw(&mut lcd)
+        .unwrap();
 
     loop {}
 }
