7aa40d78c2525c1f4a3bf0fcc48361057bfe8df4
Vitaly Domnikov <dotcypress@gmail.com>
2021-11-07 09:37:39 -0800

    update shell example

--- examples/led_shell.rs
+++ examples/led_shell.rs
@@ -15,36 +15,6 @@ use longan_nano::{
 use riscv_rt::entry;
 use ushell::{autocomplete::*, history::*, *};
 
-const MAX_COMMAND_LEN: usize = 16;
-const HISTORY_SIZE: usize = 4;
-const COMMANDS: usize = 5;
-
-const SHELL_PROMPT: &str = "#> ";
-const CR: &str = "\r\n";
-const HELP: &str = "\r\n\
-\x1b[31mL\x1b[32mE\x1b[34mD\x1b[33m Shell\x1b[0m\r\n\r\n\
-USAGE:\r\n\
-\tcommand [arg]\r\n\r\n\
-COMMANDS:\r\n\
-\ton  <ch>  Switch led channel on [r,g,b,a]\r\n\
-\toff <ch>  Switch led channel off [r,g,b,a]\r\n\
-\tstatus    Get leds status\r\n\
-\tclear     Clear screen\r\n\
-\thelp      Print this message\r\n
-";
-
-struct Context {
-    red: RED,
-    green: GREEN,
-    blue: BLUE,
-    shell: UShell<
-        ushell::Serial<u8, Tx<USART0>, Rx<USART0>>,
-        StaticAutocomplete<{ COMMANDS }>,
-        LRUHistory<{ MAX_COMMAND_LEN }, { HISTORY_SIZE }>,
-        { MAX_COMMAND_LEN },
-    >,
-}
-
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
@@ -62,6 +32,11 @@ fn main() -> ! {
     let gpioa = dp.GPIOA.split(&mut rcu);
     let gpioc = dp.GPIOC.split(&mut rcu);
 
+    let (mut red, mut green, mut blue) = rgb(gpioc.pc13, gpioa.pa1, gpioa.pa2);
+    red.off();
+    green.off();
+    blue.off();
+
     let tx = gpioa.pa9.into_alternate_push_pull();
     let rx = gpioa.pa10.into_floating_input();
 
@@ -72,90 +47,115 @@ fn main() -> ! {
     };
     let uart = serial::Serial::new(dp.USART0, (tx, rx), config, &mut afio, &mut rcu);
     let (tx, rx) = uart.split();
+    let serial = ushell::Serial::from_parts(tx, rx);
 
     let autocomplete = StaticAutocomplete(["clear", "help", "status", "off ", "on "]);
-    let history = LRUHistory::default();
-    let shell = UShell::new(ushell::Serial::from_parts(tx, rx), autocomplete, history);
-
-    let (mut red, mut green, mut blue) = rgb(gpioc.pc13, gpioa.pa1, gpioa.pa2);
-    red.off();
-    green.off();
-    blue.off();
-
-    let mut ctx = Context {
-        shell,
-        red,
-        green,
-        blue,
-    };
+    let mut shell = UShell::new(serial, autocomplete, LRUHistory::default());
+    let mut env = Env { red, green, blue };
 
     loop {
-        poll_serial(&mut ctx);
+        shell.spin(&mut env).ok();
     }
 }
 
-fn poll_serial(ctx: &mut Context) {
-    match ctx.shell.poll() {
-        Ok(Some(Input::Command((cmd, args)))) => {
-            match cmd {
-                "help" => {
-                    ctx.shell.write_str(HELP).ok();
-                }
-                "clear" => {
-                    ctx.shell.clear().ok();
-                }
-                "status" => {
-                    let red = if ctx.red.is_on() { "On" } else { "Off" };
-                    let green = if ctx.green.is_on() { "On" } else { "Off" };
-                    let blue = if ctx.blue.is_on() { "On" } else { "Off" };
-                    write!(
-                        ctx.shell,
-                        "{0:}Red: {1:}{0:}Green: {2:}{0:}Blue: {3:}{0:}",
-                        CR, red, green, blue,
-                    )
-                    .ok();
-                }
-                "on" => {
-                    match args {
-                        "r" | "red" => ctx.red.on(),
-                        "g" | "green" => ctx.green.on(),
-                        "b" | "blue" => ctx.blue.on(),
-                        "a" | "all" => {
-                            ctx.red.on();
-                            ctx.green.on();
-                            ctx.blue.on();
-                        }
-                        _ => {
-                            write!(ctx.shell, "{0:}unsupported color channel", CR).ok();
-                        }
-                    }
-                    ctx.shell.write_str(CR).ok();
-                }
-                "off" => {
-                    match args {
-                        "r" | "red" => ctx.red.off(),
-                        "g" | "green" => ctx.green.off(),
-                        "b" | "blue" => ctx.blue.off(),
-                        "a" | "all" => {
-                            ctx.red.off();
-                            ctx.green.off();
-                            ctx.blue.off();
-                        }
-                        _ => {
-                            write!(ctx.shell, "{0:}unsupported color channel", CR).ok();
-                        }
-                    }
-                    ctx.shell.write_str(CR).ok();
-                }
-                "" => {
-                    ctx.shell.write_str(CR).ok();
-                }
-                _ => {
-                    write!(ctx.shell, "{0:}unsupported command{0:}", CR).ok();
-                }
+const CMD_LEN: usize = 16;
+const HISTORY_SIZE: usize = 4;
+const COMMANDS: usize = 5;
+
+type Serial = ushell::Serial<u8, Tx<USART0>, Rx<USART0>>;
+type Autocomplete = StaticAutocomplete<{ COMMANDS }>;
+type History = LRUHistory<{ CMD_LEN }, { HISTORY_SIZE }>;
+type Shell = UShell<Serial, Autocomplete, History, { CMD_LEN }>;
+
+struct Env {
+    red: RED,
+    green: GREEN,
+    blue: BLUE,
+}
+
+type EnvResult = SpinResult<Serial, ()>;
+
+impl Env {
+    fn on_cmd(&mut self, shell: &mut Shell, args: &str) -> EnvResult {
+        match args {
+            "r" | "red" => self.red.on(),
+            "g" | "green" => self.green.on(),
+            "b" | "blue" => self.blue.on(),
+            "a" | "all" => {
+                self.red.on();
+                self.green.on();
+                self.blue.on();
             }
-            ctx.shell.write_str(SHELL_PROMPT).ok();
+            _ => {
+                write!(shell, "{0:}unsupported color channel", CR).ok();
+            }
         }
-        _ => {}
+        shell.write_str(CR)?;
+        Ok(())
+    }
+
+    fn off_cmd(&mut self, shell: &mut Shell, args: &str) -> EnvResult {
+        match args {
+            "r" | "red" => self.red.off(),
+            "g" | "green" => self.green.off(),
+            "b" | "blue" => self.blue.off(),
+            "a" | "all" => {
+                self.red.off();
+                self.green.off();
+                self.blue.off();
+            }
+            _ => {
+                write!(shell, "{0:}unsupported color channel", CR).ok();
+            }
+        }
+        shell.write_str(CR)?;
+        Ok(())
+    }
+
+    fn status_cmd(&mut self, shell: &mut Shell, _args: &str) -> EnvResult {
+        let red = if self.red.is_on() { "On" } else { "Off" };
+        let green = if self.green.is_on() { "On" } else { "Off" };
+        let blue = if self.blue.is_on() { "On" } else { "Off" };
+        write!(
+            shell,
+            "{0:}Red:   {1:}{0:}Green: {2:}{0:}Blue:  {3:}{0:}",
+            CR, red, green, blue,
+        )?;
+
+        Ok(())
     }
 }
+
+impl Environment<Serial, Autocomplete, History, (), { CMD_LEN }> for Env {
+    fn command(&mut self, shell: &mut Shell, cmd: &str, args: &str) -> EnvResult {
+        match cmd {
+            "clear" => shell.clear()?,
+            "help" => shell.write_str(HELP)?,
+            "status" => self.status_cmd(shell, args)?,
+            "on" => self.on_cmd(shell, args)?,
+            "off" => self.off_cmd(shell, args)?,
+            "" => shell.write_str(CR)?,
+            _ => write!(shell, "{0:}unsupported command: \"{1:}\"{0:}", CR, cmd)?,
+        }
+        shell.write_str(SHELL_PROMPT)?;
+        Ok(())
+    }
+
+    fn control(&mut self, _shell: &mut Shell, _code: u8) -> EnvResult {
+        Ok(())
+    }
+}
+
+const SHELL_PROMPT: &str = "#> ";
+const CR: &str = "\r\n";
+const HELP: &str = "\r\n\
+\x1b[31mL\x1b[32mE\x1b[34mD\x1b[33m Shell\x1b[0m\r\n\r\n\
+USAGE:\r\n\
+\x20 command [arg]\r\n\r\n\
+COMMANDS:\r\n\
+\x20 on  <ch>  Switch led channel on [r,g,b,a]\r\n\
+\x20 off <ch>  Switch led channel off [r,g,b,a]\r\n\
+\x20 status    Get leds status\r\n\
+\x20 clear     Clear screen\r\n\
+\x20 help      Print this message\r\n
+";
